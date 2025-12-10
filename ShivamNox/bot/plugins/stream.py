7b5e420282f51c6276e1880061c10d2072ca8fb3
vinit000033 <vinit000033@gmail.com>
2025-12-10 12:38:25 +0530

    Update stream.py

--- ShivamNox/bot/plugins/stream.py
+++ ShivamNox/bot/plugins/stream.py
@@ -73,7 +73,7 @@ async def private_receive_handler(c: Client, m: Message):
             if user.status == "kicked":
                 await c.send_message(
                     chat_id=m.chat.id,
-                    text="You are banned!\n\n  **Cᴏɴᴛᴀᴄᴛ Support [Support](https://t.me/pulsehub) They Wɪʟʟ Hᴇʟᴘ Yᴏᴜ**",
+                    text="You are banned!\n\n  **Cᴏɴᴛᴀᴄᴛ Support [Support](https://t.me/pulsehub1) They Wɪʟʟ Hᴇʟᴘ Yᴏᴜ**",
                     disable_web_page_preview=True
                 )
                 return
@@ -94,7 +94,7 @@ async def private_receive_handler(c: Client, m: Message):
             await m.reply_text(e)
             await c.send_message(
                 chat_id=m.chat.id,
-                text="**Sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ Wʀᴏɴɢ. Cᴏɴᴛᴀᴄᴛ ᴍʏ Support** [Support](https://t.me/pulsehub)",
+                text="**Sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ Wʀᴏɴɢ. Cᴏɴᴛᴀᴄᴛ ᴍʏ Support** [Support](https://t.me/pulsehub1)",
                 disable_web_page_preview=True
             )
             return
